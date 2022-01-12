f2e28afad0f506b4dd03188d8c99fbc2363eae25
Nyarla123 <rksekf456@gmail.com>
2022-01-12 16:46:30 +0900

    Second Commit

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,19 +1,55 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    protected void configure(HttpSecurity http) throws Exception {
+    private final MemberService memberService;
 
+    public SecurityConfig(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/") // 로그인 성공시 이동 url
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher
+                        ("/members/logout"))
+                .logoutSuccessUrl("/");
+
+        http.authorizeRequests()
+                .mvcMatchers("/","/members/**",
+                        "/item/**", "/images/**").permitAll() // 이 경로는 로그인 없이 접근가능
+                .mvcMatchers("/admin/**").hasRole("ADMIN") // ADMIN Role만 접근가능
+                .anyRequest().authenticated(); // 위에 설정해준 경로를 제외한 나머지경로 모두 인증 요구 설정
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // static directory 하위 파일 인증 무시
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     @Bean
@@ -21,4 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
 }
